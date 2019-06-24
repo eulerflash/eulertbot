014e48e95ba4399a5be17ba9e6c0dd0bfc3abab3
Qioki <52178756+Qioki@users.noreply.github.com>
2019-06-25 00:25:01 +0300

    Create tbot.py

---
+++ tbot.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot = telebot.TeleBot("629362377:AAH9qfUuwkzw-qIOiEOx4fL3ZHgXgD9yykA")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+    bot.reply_to(message, message.text)
+
+bot.polling( none_stop = True )
